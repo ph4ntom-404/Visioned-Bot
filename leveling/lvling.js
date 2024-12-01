e03b85d4c2f5c6972fd27fc1219da6d6ec893388
ph4ntom-404 <oordankale@gmail.com>
2024-12-01 01:26:19 -0600

    fixed leveling multiplier

--- leveling/lvling.js
+++ leveling/lvling.js
@@ -27,7 +27,7 @@ module.exports = (async(user,message)=>{
         .setThumbnail('https://media.discordapp.net/attachments/1309351299191410748/1311783223415799920/35D622D4-FE4D-4150-9EE2-F91E6D18C899.gif?ex=674a1cd4&is=6748cb54&hm=1a71ae12818bb5562d3f273f5feb6a27922906b5ef1108cdf3897d1e62c6cb33&=')
         .setFooter({text:'Meruem',iconURL:message.author.displayAvatarURL()});
         await message.reply({embeds:[embed]});
-        user.goal = Math.floor((user.goal * 1.5)/10) * 10;
+        user.goal = Math.floor((user.goal * 1.5));
     }
     await user.save()
     return;
