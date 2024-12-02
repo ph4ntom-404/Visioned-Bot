44ec60b4d732f99237581ecb36d390fec971ef5e
ph4ntom-404 <oordankale@gmail.com>
2024-12-02 17:19:27 -0600

    added health check path

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Bot is running!'));
+app.get('/health', (req, res) => {
+    res.status(200).send('Bot is running!');
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
