90292950edd8c5f99bd3e568211affee6f6afbf7
ph4ntom-404 <oordankale@gmail.com>
2024-12-02 17:53:12 -0600

    changed link

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(port, () => console.log(`Server is listening on port ${port}`));
 const axios = require('axios');
 
 // URL of your bot's endpoint
-const botUrl = 'https://treasures-bot.onrender.com/health';
+const botUrl = 'https://meruem.onrender.com/health';
 
 // Function to ping the bot
 function keepAlive() {
